7b98d43b825d1b8802a23398de5d9157eac3b8af
Ivan Li <ivan.li@hotmail.com>
2017-10-12 00:31:35 +1100

    added register test

--- tests/Browser/Auth/LoginTest.php
+++ tests/Browser/Auth/LoginTest.php
@@ -18,7 +18,7 @@ class LoginTest extends DuskTestCase
     }
 
     /**
-     * A Dusk test example.
+     * Test elements on the page
      *
      * @return void
      */
@@ -33,24 +33,60 @@ class LoginTest extends DuskTestCase
         });
     }
 
+    /**
+     * Test login validation
+     *
+     * @return void
+     */
     public function testLoginFormValidation()
     {
         $this->browse(function (Browser $browser) {
-            $browser->click('form button')
+            $browser->visit('/auth/login')
+                ->click('form button')
                 ->waitForText('email field is required', 5)
                 ->assertSee('email field is required')
                 ->assertSee('password field is required');
         });
     }
 
+    /**
+     * Test login submission
+     *
+     * @return void
+     */
     public function testLoginFormSubmission()
     {
         $this->browse(function (Browser $browser) {
             $user = factory(User::class)->create();
-            $browser->type('email', $user->email)
+            $browser->visit('/auth/login')
+                ->type('email', $user->email)
                 ->type('password', 'secret')
-                ->pressAndWaitFor('Login')
-                ->assertPathIs('/');
+                ->press('Login')
+                ->assertRouteIs('home');
+        });
+    }
+
+    /**
+     * Test authenticated user visiting login page
+     */
+    public function testAuthenticatedLoginPageRedirect()
+    {
+        $this->browse(function (Browser $browser) {
+            $user = factory(User::class)->create();
+            $browser->loginAs($user)
+                ->visit(route('auth.login.get'))
+                ->pause(1000)
+                ->assertRouteIs('home')
+                ->logout();
+        });
+    }
+
+    public function testUnauthenticatedHomePageRedirect()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('home'))
+                ->pause(1000)
+                ->assertRouteIs('auth.login.get');
         });
     }
 }
