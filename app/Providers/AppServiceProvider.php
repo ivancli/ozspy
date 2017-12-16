3019c814657b7364d6a8f62a6aebd6ea41f5ed53
Ivan Li <ivan.li@hotmail.com>
2017-12-16 16:43:44 +1100

    added query log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace OzSpy\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +17,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        \DB::listen(function ($query) {
+            if ($query->time > 10) {
+                $view_log = new Logger('Query Log');
+                $view_log->pushHandler(new StreamHandler(storage_path('logs/query.log'), Logger::INFO));
+                $view_log->addInfo("Slow query run on DB", [
+                    'date' => Carbon::now(),
+                    'sql' => $query->sql,
+                    'bindings' => $query->bindings,
+                    'time' => $query->time
+                ]);
+            }
+        });
     }
 
     /**
