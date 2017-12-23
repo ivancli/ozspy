03f326cbb77d40f1a30ffe12d457d6061722b7e5
Ivan Li <ivan.li@hotmail.com>
2017-12-23 20:27:01 +1100

    added scraper to storage

---
+++ storage/app/scraper/index.js
@@ -0,0 +1,34 @@
+let argvs = {};
+
+process.argv.forEach((argv) => {
+    if (argv.indexOf('=') > -1) {
+        let element = argv.split('=');
+        if (element.length === 2) {
+            let key = element[0].replace('--', '');
+            argvs[key] = element[1];
+        }
+    }
+});
+
+if (!argvs.scraper) {
+    argvs.scraper = 'categories';
+}
+
+if (!argvs.id) {
+    throw new Error('Please provide target ID for output purpose.');
+}
+
+if (!argvs.url) {
+    throw new Error('Please provide URL for crawling purpose.');
+}
+
+if (!argvs.retailer) {
+    throw new Error('Please provide retailer for crawling purpose.');
+}
+
+let Scraper = require('./scrapers/' + argvs.retailer + '/' + argvs.scraper);
+
+let scraper = new Scraper(argvs.id, argvs.url);
+scraper.scrape();
+let products = scraper.products;
+console.info(products);
